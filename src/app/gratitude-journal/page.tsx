abc24f3739d3eaa398fbf90062278430b1f647b6
BillyBobElroy <william.forrester@hotmail.com>
2025-04-26 19:07:17 -0400

    Update: Minor Updates (2)

--- src/app/gratitude-journal/page.tsx
+++ src/app/gratitude-journal/page.tsx
@@ -108,6 +108,22 @@ export default function GratitudeJournal() {
           <p className="text-center text-zinc-400">No entries for this date.</p>
         )}
       </div>
+      <div className="my-8 flex justify-center">
+            <ins
+              className="adsbygoogle"
+              style={{ display: 'inline-block', width: '728px', height: '90px' }}
+              data-ad-client="ca-pub-4813693653154178"
+              data-ad-slot="8997853730"
+              data-ad-format="auto"
+              data-full-width-responsive="true"
+            />
+            <ins
+              className="adsbygoogle inline-block md:hidden"
+              style={{ width: '300px', height: '250px' }}
+              data-ad-client="ca-pub-4813693653154178"
+              data-ad-slot="8997853730"
+            />
+      </div>
     </div>
   );
 }
