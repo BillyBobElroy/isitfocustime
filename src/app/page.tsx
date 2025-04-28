b7c6ecfb611fcc8affc1b95bde25eddec7a942e6
BillyBobElroy <william.forrester@hotmail.com>
2025-04-28 09:08:47 -0400

    Updated: Minor fixes (3)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -112,7 +112,7 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen bg-zinc-900 text-white px-4">
               <div className="max-w-md w-full space-y-8">
-              <div className="text-center"></div>
+              <div className="text-center">
       {isBreathing ? (
         <BreatheTimer />
       ) : (
@@ -203,6 +203,7 @@ export default function Home() {
           </div>
           </>
       )}
+      </div>
       </div>
     </main>
   );
