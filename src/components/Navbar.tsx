239a17c699275a5b1523cac1cc2c54db50d51899
BillyBobElroy <william.forrester@hotmail.com>
2025-04-28 08:05:02 -0400

    Updated: Minor fixes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { useState } from 'react';
-import { Menu, X } from 'lucide-react'; // You can swap icons if you want
+import { Menu, X } from 'lucide-react'; // You can swap icons if you want 
 
 export default function Navbar() {
   const pathname = usePathname();
